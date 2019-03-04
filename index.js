140614556d60d7f19ae0cc986a0b465f2b70e0bc
Lukasz Tarasiewicz <lukasz.tarasiewicz86@gmail.com>
2019-03-04 12:06:13 +0100

    Work with command line arguments

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const fs = require("fs");
 const lcloudBucket = "lcloud-427-lt";
 const pathToUploadFile = "./file-to-upload";
 
+/*
+ Put file to the S3 bucket to have something to work with
+ */
 fs.readFile(pathToUploadFile, (err, data) => {
     s3.putObject({Bucket: lcloudBucket, Key: "lt-file", Body: data}, function(err, data) {
         if (err)
@@ -17,27 +20,17 @@ fs.readFile(pathToUploadFile, (err, data) => {
 });
 
 
-/*s3.listObjectsV2({Bucket: lcloudBucket}, (err, data) => {
-  console.log(data.Contents);
-  console.log(err);
-});
+const deleteObjectByKey = (key) => {
+  return new Promise((resolve, _) => {
+      s3.deleteObject({Bucket: lcloudBucket, Key: key}, (_, data) => {
+          resolve(data);
+      });
+  });
+};
 
-
-s3.listObjectsV2({Bucket: lcloudBucket, Prefix: "lt-"}, (err, data) => {
-    console.log(data.Contents);
-    console.log(err);
-});
-
-s3.deleteObject({Bucket: lcloudBucket, Key: "lt-file"}, (err, data) => {
-    console.log(data.Contents);
-    console.log(err);
-});*/
 
 const listFileMatchingOptionalRegexp = (regexp = /.+/) => {
-    {
-
-    }
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve, _) => {
         let filteredObjects = [];
         s3.listObjectsV2({Bucket: lcloudBucket}, (_, data) => {
             for (let s3Object of data.Contents) {
@@ -50,7 +43,11 @@ const listFileMatchingOptionalRegexp = (regexp = /.+/) => {
     });
 };
 
-
-listFileMatchingOptionalRegexp(). then((s3ObjectsMatchingFilter) => {
+/*
+ Read the regexp pattern from the command line (first argument give to the script)
+ */
+listFileMatchingOptionalRegexp(process.argv[2]). then((s3ObjectsMatchingFilter) => {
     console.log(s3ObjectsMatchingFilter);
 });
+
+
