3be39f3633860fe5b6b5e0e2baf37fd1de33f107
Lukasz Tarasiewicz <lukasz.tarasiewicz86@gmail.com>
2019-03-04 12:10:53 +0100

    Upload more files

--- index.js
+++ index.js
@@ -5,19 +5,22 @@ const s3 = new AWS.S3();
 const fs = require("fs");
 
 const lcloudBucket = "lcloud-427-lt";
-const pathToUploadFile = "./file-to-upload";
+const pathsToUploadFiles = ["./file-to-upload", "./second-file-to-upload"];
 
 /*
- Put file to the S3 bucket to have something to work with
+ Put files to the S3 bucket to have something to work with
  */
-fs.readFile(pathToUploadFile, (err, data) => {
-    s3.putObject({Bucket: lcloudBucket, Key: "lt-file", Body: data}, function(err, data) {
-        if (err)
-            console.log(err);
-        else
-            console.log("Successfully uploaded data to " + lcloudBucket);
+pathsToUploadFiles.forEach((filePath, index) => {
+    fs.readFile(filePath, (err, data) => {
+        s3.putObject({Bucket: lcloudBucket, Key: `lt-file-${index}`, Body: data}, function(err, _) {
+            if (err) {
+               throw Error("Error uploading files");
+            }
+            console.log(`Successfully uploaded ${filePath} to ${lcloudBucket}`);
+        });
     });
 });
+
 
 
 const deleteObjectByKey = (key) => {
